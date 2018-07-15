5a2978e7c55e74cd45c52773ff668c6443c30a5b
Aditya Pandey <addherbs@gmail.com>
2018-07-15 03:40:21 -0700

    Daily temperatures

---
+++ Medium/daily_temperatures.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        ans = [0] * len(temperatures)
+        stack = []
+        for i, t in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < t:
+                cur = stack.pop()
+                ans[cur] = i - cur
+            stack.append(i)
+
+        return ans
