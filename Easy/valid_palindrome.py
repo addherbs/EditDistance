7e063fda3ae4463cd18cda73b2d3598632c15d8f
Aditya Pandey <addherbs@gmail.com>
2018-07-21 13:52:04 -0500

    Check if a string is a valid palindrome or not

---
+++ Easy/valid_palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = "".join([x for x in s if (x.isalnum())]).lower()
+        return s == s[::-1]
