2da93e2aece1c239160f67fac4267783216bb93f
Aditya Pandey <addherbs@gmail.com>
2020-11-16 18:44:32 -0600

    Find Kth largest using min heap approach

---
+++ Explore_Google/Kth Largest Element in an Array.py
@@ -0,0 +1,35 @@
+"""
+Kth Largest Element in an Array
+
+Solution
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+Note:
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+"""
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        from heapq import heappush, heappop
+        
+        heap = []
+        
+        for elem in nums:
+            if len(heap) < k:
+                heappush(heap, elem)
+            else:
+                val = heappop(heap)
+                if elem > val:
+                    heappush(heap, elem)
+                else:
+                    heappush(heap, val)
+                    
+        return heappop(heap)
