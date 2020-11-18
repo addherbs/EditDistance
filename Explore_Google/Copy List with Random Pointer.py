4e6bcd67e9ceb3b696450cce9f863d7bafc6c633
Aditya Pandey <addherbs@gmail.com>
2020-11-17 20:28:50 -0600

    Copy list with random pointer, Space O(1) solution

--- Explore_Google/Copy List with Random Pointer.py
+++ Explore_Google/Copy List with Random Pointer.py
@@ -58,6 +58,7 @@ class Node:
         self.random = random
 """
 
+# O(N) space solution
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
         vis = {}
@@ -88,3 +89,47 @@ class Solution:
         return temp
     
     
+
+# O(1) space solution
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+
+        if not head: return None
+
+        pointer = head
+
+        while pointer:
+
+            new_node = Node(pointer.val)
+            new_node.next = pointer.next
+            pointer.next = new_node
+
+            pointer = new_node.next
+
+        pointer = head
+
+        while pointer:
+            pointer.next.random = pointer.random.next if pointer.random else None
+            pointer = pointer.next.next
+
+        old_list = head
+        new_list = head.next
+        ans = new_list
+
+        while old_list:
+            old_list.next = old_list.next.next
+            new_list.next = new_list.next.next if new_list.next else None
+            old_list = old_list.next
+            new_list = new_list.next
+
+        return ans
