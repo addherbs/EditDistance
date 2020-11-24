ce37efea029a3b158457877af536ec50dc77c244
Aditya Pandey <addherbs@gmail.com>
2020-11-24 05:58:58 -0600

    Range Sum Query 2D Mutable, O(N) time complexity for sumRegion function using prefix sum

--- Explore_Google/Range Sum Query 2D - Mutable.py
+++ Explore_Google/Range Sum Query 2D - Mutable.py
@@ -51,3 +51,35 @@ class NumMatrix:
 # obj = NumMatrix(matrix)
 # obj.update(row,col,val)
 # param_2 = obj.sumRegion(row1,col1,row2,col2)
+
+
+# O(N) time complexity for sumRegion function
+class NumMatrix:
+    
+    def __init__(self, matrix: List[List[int]]):
+        if not matrix:
+            self.mat, self.m, self.n = [[]], 0, 0
+        else:
+            self.mat = [matrix[i][:] for i in range(len(matrix))]
+            self.prefix_mat = [self.calc_prefix(row) for row in matrix]
+            self.m, self.n = len(matrix), len(matrix[0])
+        
+    def update(self, row: int, col: int, val: int) -> None:
+        if 0 <= row < self.m and 0 <= col < self.n:
+            self.mat[row][col] = val
+            self.prefix_mat[row] = self.calc_prefix(self.mat[row])
+            
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        ans = 0
+        for i in range(row1, row2 + 1):
+            ans += (self.prefix_mat[i][col2]) - (0 if col1 == 0 else self.prefix_mat[i][col1 - 1])
+        return ans
+
+    def calc_prefix(self, row):
+        prefix = []
+        ans = 0
+        for val in row:
+            ans+= val
+            prefix.append(ans)
+        return prefix
